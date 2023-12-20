fb2c6a26b490deec2421e35675cb321b0bd650c3
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-12-19 18:59:01 -0800

    trying

---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
+#endif
